3500c943d2597f1feb7a4bcc9baed6e7900b17e0
AlexNix <alexnix.mail@gmail.com>
2022-07-23 23:27:41 +0300

    create-layout-component

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import Layout from "../component/Layout";
+import Head from "next/head";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+	return (
+		<>
+			<Head>
+				<title>Candleaf Shop</title>
+			</Head>
+			<Layout>
+				<Component {...pageProps} />
+			</Layout>
+		</>
+	);
 }
 
-export default MyApp
+export default MyApp;
