f839bfc00017a77879ec86c2c97d6d49859dcb9c
AlexNix <alexnix.mail@gmail.com>
2022-07-29 21:38:45 +0300

    snuggle-footer

--- component/Layout.jsx
+++ component/Layout.jsx
@@ -1,13 +1,14 @@
 import Header from "./Header";
 import Footer from "./Footer";
+import styles from "../styles/Layout.module.scss";
 
 const Layout = ({ children }) => {
 	return (
-		<>
+		<div className={styles.wrapper}>
 			<Header />
-			<main>{children}</main>
+			<main className={styles.content}>{children}</main>
 			<Footer />
-		</>
+		</div>
 	);
 };
 
