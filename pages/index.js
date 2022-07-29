3fab8244222b57193e10b0698fb5ac606430f786
AlexNix <alexnix.mail@gmail.com>
2022-07-29 21:41:31 +0300

    import-section-in-index.js-file

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,15 @@
 import Banner from "../component/Banner";
 import ProductsGrid from "../component/ProductsGrid";
+import Info from "../component/Info";
+import Comment from "../component/Comment";
 
 const Home = () => {
 	return (
 		<>
 			<Banner />
 			<ProductsGrid />
+			<Info />
+			<Comment />
 		</>
 	);
 };
