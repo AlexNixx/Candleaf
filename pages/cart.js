68f263d21d5a01cafd0d929e70bf49f64a011082
AlexNix <alexnix.mail@gmail.com>
2022-08-02 23:23:58 +0300

    create-cart-page

---
+++ pages/cart.js
@@ -0,0 +1,85 @@
+import Link from "next/link";
+import Image from "next/image";
+import styles from "../styles/Cart.module.scss";
+import { useStateContext } from "../lib/context";
+
+const Cart = () => {
+	const { cartItems, onAdd, onRemove, totalPrice } = useStateContext();
+
+	console.log(cartItems);
+
+	return (
+		<div className={styles.wrapper}>
+			<h1 className={styles.title}>Your cart items</h1>
+			<Link href="/">
+				<p className={styles.link}>Back to shopping</p>
+			</Link>
+			{cartItems.length >= 1 ? (
+				<div>
+					<table className={styles.table}>
+						<thead>
+							<tr>
+								<th>Product</th>
+								<th>Price</th>
+								<th>Quantity</th>
+								<th>Total</th>
+							</tr>
+						</thead>
+						<tbody>
+							{cartItems.map((product) => (
+								<tr>
+									<td className={styles.product}>
+										<div className={styles.img_container}>
+											<Image
+												src={product.image.data.attributes.formats.medium.url}
+												height={100}
+												width={70}
+											/>
+										</div>
+										<div className={styles.product_info}>
+											<h1 className={styles.product_title}>
+												{product.title} Candleaf®
+											</h1>
+											<button className={styles.button_remove}>Remove</button>
+										</div>
+									</td>
+									<td className={styles.price}>
+										<span>$ {product.price}</span>
+									</td>
+									<td className={styles.qty}>
+										<div className={styles.qty_container}>
+											<button onClick={() => onRemove(product)}>-</button>
+											<span>{product.quantity}</span>
+											<button onClick={() => onAdd(product, 1)}>+</button>
+										</div>
+									</td>
+									<td className={styles.total}>
+										<span>
+											$ {(product.price * product.quantity).toFixed(2)}
+										</span>
+									</td>
+								</tr>
+							))}
+						</tbody>
+					</table>
+					<div className={styles.check_out}>
+						<div className={styles.container}>
+							<div className={styles.total}>
+								<h1>Sub-total</h1>
+								<span>$ {totalPrice.toFixed(2)}</span>
+							</div>
+							<p className={styles.desc}>
+								Tax and shipping cost will be calculated later
+							</p>
+						</div>
+						<button className={styles.button_check_out}>Check-out</button>
+					</div>
+				</div>
+			) : (
+				<span>Order smng</span>
+			)}
+		</div>
+	);
+};
+
+export default Cart;
